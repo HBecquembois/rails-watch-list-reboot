987a1de8de5e12281c83f852fc1436c27186d181
Helene Becquembois <hbecquembois@gmail.com>
2023-06-27 12:04:04 +0200

    generated bookmarks controller and defined new, create and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists, only: [ :index, :show, :new, :create]
+  resources :lists, only: [:index, :create, :new, :show] do
+    resources :bookmarks, only: [:create, :new]
+  end
+  resources :bookmarks, only: [:destroy]
 end
+
