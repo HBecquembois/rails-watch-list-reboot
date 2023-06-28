ebbeb7d5397354643b82c5bc6e8557368888cdc9
Helene Becquembois <hbecquembois@gmail.com>
2023-06-28 11:14:59 +0200

    added cleaning database actions in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "open-uri"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,6 +6,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "cleaning database"
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
 
 puts 'creating 4 movies'
 
